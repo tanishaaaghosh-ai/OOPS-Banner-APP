da0614358a1c0d5859b9aa7b9a50719b40560670
tanishaaaghosh-ai <tanishaaaghosh@gmail.com>
2026-03-11 09:24:59 +0530

    Ascii art

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-public class Main{
- public static void main(String args[]){
-     System.out.println("OOPS");
+public class Main {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("", "  ***** ", "  ***** ", " ******* ", "  ****** "),
+            String.join("", " * * ", " * * ", " * * ", " * "),
+            String.join("", " * * ", " * * ", " * * ", " * "),
+            String.join("", " * * ", " * * ", " ******* ", "  ***** "),
+            String.join("", " * * ", " * * ", " * ", "       * "),
+            String.join("", " * * ", " * * ", " * ", "       * "),
+            String.join("", "  ***** ", "  ***** ", " * ", " ****** ")
+        };
+
+     
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
