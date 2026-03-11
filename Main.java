869f1f444cdb764e79bf2bcfd68d0d1c63afa440
tanishaaaghosh-ai <tanishaaaghosh@gmail.com>
2026-03-11 09:33:23 +0530

    Used string join

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(String.join(" ", "   *   ", "   *   ", "**   ", "     ** "));
-        System.out.println(String.join(" ", " *   * ", " *   * ", "*   * ", " **      "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*    *", "**       "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*   * ", " **      "));
-        System.out.println(String.join(" ", "*     *", "*     *", "**   ", "   **   "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "     ** "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "      **"));
-        System.out.println(String.join(" ", " *   * ", " *   * ", "*      ", "      ** "));
-        System.out.println(String.join(" ", "   *   ", "   *   ", "*      ", "   ** "));
+
+        String[] banner = new String[9];
+
+        banner[0] = String.join("", "  * ",  "   * ",  " * ",  "  * ");
+        banner[1] = String.join("", " *  * ", " *  * ", " *  * ", " *   ");
+        banner[2] = String.join("", " *  * ", " *  * ", " *  * ", " *   ");
+        banner[3] = String.join("", " *  * ", " *  * ", " *  ", " *  ");
+        banner[4] = String.join("", " *  * ", " *  * ", " *    ", "  *");
+        banner[5] = String.join("", " *  * ", " *  * ", " *    ", "     * ");
+        banner[6] = String.join("", " *  * ", " *  * ", " *    ", "     * ");
+        banner[7] = String.join("", " *  * ", " *  * ", " *    ", " *   * ");
+        banner[8] = String.join("", "  *  ", "   *  ", " *    ", "  * ");
+
+        System.out.println("--- OOPS Banner ---\n");
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
