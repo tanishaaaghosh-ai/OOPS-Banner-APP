be7ff847b8d4fad156c0f0e4ef9a704a8ce76c78
tanishaaaghosh-ai <tanishaaaghosh@gmail.com>
2026-03-11 09:37:55 +0530

    Used class  optimisation

--- Main.java
+++ Main.java
@@ -1,20 +1,50 @@
 public class Main {
+   public static String[] O() {
+        return new String[]{
+                "  ***  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  ***  "
+        };
+    }
+
+    public static String[] P() {
+        return new String[]{
+                "  ** ",
+                " *     * ",
+                " *     * ",
+                "  ** ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    public static String[] S() {
+        return new String[]{
+                "  ** ",
+                " *       ",
+                " *       ",
+                "  ***  ",
+                "       * ",
+                "       * ",
+                "  ** "
+        };
+    
+}
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("", "  ** ", "  ** ", " *** ", "  ** "),
-            String.join("", " * * ", " * * ", " * * ", " * "),
-            String.join("", " * * ", " * * ", " * * ", " * "),
-            String.join("", " * * ", " * * ", " *** ", "  *** "),
-            String.join("", " * * ", " * * ", " * ", "       * "),
-            String.join("", " * * ", " * * ", " * ", "       * "),
-            String.join("", "  ** ", "  ** ", " * ", "    ** ")
-        };
+        String[] O = O();
+        String[] P = P();
+        String[] S = S();
 
-     
+        System.out.println("\n--- OOPS Banner (UC6) ---\n");
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + " " +O[i]+ " " + P[i] + " " + S[i]);
         }
     }
 }
